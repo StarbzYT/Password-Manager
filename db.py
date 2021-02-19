0b08a1d54a0a2d45f018a39f55cc4dd589915091
Your Name <you@example.com>
2021-02-19 13:21:18 -0800

    make database

---
+++ db.py
@@ -0,0 +1,45 @@
+import sqlite3
+
+
+class Database:
+    def __init__(self, db):
+        self.connection = sqlite3.connect(db)
+        self.cursor = self.connection.cursor()
+        # create passwords table ONLY IF it does not exist (creates one time)
+        self.cursor.execute(
+            "CREATE TABLE IF NOT EXISTS passwords (id INTEGER PRIMARY KEY, website text, password_ text)")
+        self.connection.commit()
+
+    # insert website and password
+
+    def insert_data(self, website, password):
+        self.cursor.execute("INSERT INTO passwords VALUES(NULL, ?, ?)",
+                            (website, password))
+        self.connection.commit()
+
+    # get the websites and passwords
+
+    def get_data(self):
+        self.cursor.execute("SELECT * FROM passwords")
+        rows = self.cursor.fetchall()  # get all data from passwords table
+        return rows  # return website, password_
+
+    # remove specific data i.e website AND password in database (grab using id)
+
+    def delete_data(self, id):
+        # grab id and it will delete BOTH website and password associated with it
+        # one item tuple requires comma
+        self.cursor.execute("DELETE FROM passwords WHERE id=?", (id,))
+        self.connection.commit()
+
+    # update with new website and password
+    def update_data(self, id, new_website, new_password):
+        # update data
+        self.cursor.execute(
+            "UPDATE passwords SET website=?, password_=? WHERE id=?",
+            (new_website, new_password, id))
+        self.connection.commit()  # ALWAYS COMMIT CHANGES
+
+
+db = Database("passwords")
+print(db.get_data())
