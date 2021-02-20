d914e61aefe14b577a0d0a3935a05212c31b76b3
Your Name <you@example.com>
2021-02-20 10:29:28 -0800

    add text and input boxes

--- password_manager.py
+++ password_manager.py
@@ -6,13 +6,32 @@ database = Database("passwords.db")
 
 
 class Application:
+    window = Tk()  # mainframe/root
+
     def __init__(self):
-        window = Tk()  # mainframe/root
-        window.title("Password Manager")
-        window.geometry("750x600")
+        Application.window.title("Password Manager")
+        Application.window.geometry("650x500")
         icon = PhotoImage(file='lock-icon.png')
         # Setting icon png type file
-        window.iconphoto(False, icon)
+        Application.window.iconphoto(False, icon)
+        self.widgets()  # run widgets method to get all widgets
+
+    # create widgets
+    def widgets(self):
+        website_label = Label(Application.window,
+                              text="Website", font=("calibre", 12, "bold"))
+        website_label.grid(column=0, row=0)
+        # input box for website
+        website = Entry(Application.window, font=("calibre", 12, "normal"))
+        website.grid(column=1, row=0)
+        password_label = Label(
+            Application.window, text="Password", font=("calibre", 12, "bold"))
+        password_label.grid(column=2, row=0)
+        # input box for password
+        password = Entry(Application.window, font=("calibre", 12, "normal"))
+        password.grid(column=3, row=0)
+        # buttons
 
 
 app = Application()
+Application.window.mainloop()
